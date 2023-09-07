5b15ec740ed91e70de5eb0ebe0340b8ed4425ede
sultan9754 <143875285+sultan9754@users.noreply.github.com>
2023-09-07 11:03:41 +0700

    Add files via upload

---
+++ coding genap.py
@@ -0,0 +1,27 @@
+# program menghitung volume balok
+print("program menghitung volume balok")
+panjang = int(input("masukkan panjang balok =  "))
+lebar = int(input("masukkan lebar balok = "))
+tinggi = int(input("masukkan tinggi balok = "))
+volume = panjang * lebar * tinggi
+print("volume = ", volume)
+
+
+
+# program menghitung volume tabung
+print("program menghitung volume tabung")
+phi = 22/7
+r = int(input("masukkan jari-jari = "))
+tinggi = int(input("masukkan tinggi tabung = "))
+volume = phi * r**2 * tinggi
+print("volume = ", volume)
+
+
+
+# program menghitung volume bola
+print("program menghitung volume bola")
+phi = 22/7
+r = int(input("masukan jari-jari bola = "))
+volume = 4/3 * phi * r**3
+print("volume = ", volume)
+
